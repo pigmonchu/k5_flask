4c33adbcce30e0313b0bc821030bc8aba14fc215
Ramón Maldonado Rodríguez <monterdi@gmail.com>
2019-07-23 00:02:55 +0200

    CRUD completa con ficheros y formularios y errores a capon

--- app/views.py
+++ app/views.py
@@ -6,7 +6,6 @@ from os import remove, rename
 ficheromovimientos = 'data/movimientos.txt'
 ficheronuevo = 'data/nuevomovimientos.txt'
 
-
 @app.route('/')
 def index():
     #leer movimientos
@@ -42,16 +41,32 @@ def compra():
         fMovimientos.close()
         return redirect(url_for('index'))
 
-@app.route('/modificar', method=['GET', 'POST'])
-def update()
-    registro_leido <- movimientos.txt[request.values('ix')]
+@app.route('/modificar', methods=['GET', 'POST'])
+def update():
+    
     if request.method == 'GET':
-        render.template('update.html', movimientos_seleccionados = registroleido)
-        # recordar que la nueva template tendra values en los inputs del formulario
-        # value="{{movimiento_seleccionado['fecha']}}"
+        if request.values.get('ix'):
+            movimiento, ix = recuperarregistro(request.values.get('ix'))
+            return render_template('update.html', registro_seleccionado=movimiento, ix=ix)
     else:
-        recuperar los values
-        grabarlos en la posición adecuada del fichero sustituyendo al registro original (el 3)
+        if request.values.get('ix'):
+            msg = validar(request.values)
+            if msg != True:
+                registro_seleccionado = [
+                    request.values['fecha'],
+                    request.values['concepto'],
+                    request.values['monedaComprada'],
+                    request.values['cantidadComprada'],
+                    request.values['monedaPagada'],
+                    request.values['cantidadPagada']
+                ]
+
+                return render_template('update.html', registro_seleccionado=registro_seleccionado, ix=request.values['ix'], errores=msg)
+            
+            modificarregistro(request.values)
+            return redirect(url_for('index'))
+        #        recuperar los values
+        #grabarlos en la posición adecuada del fichero sustituyendo al registro original (el 3)
 
 @app.route('/procesarregistro', methods=['POST'])
 def procesar():
@@ -59,11 +74,54 @@ def procesar():
         if request.values['btnselected'] == 'Borrar':
             borrar(int(request.values['ix']))
         else:
-            modificar(int(request.values['ix']))
-            redirect(url_for('update', ix=request.values['ix']))
+            #modificar(int(request.values['ix']))
+            return redirect(url_for('update', ix=request.values['ix']))
     return redirect(url_for('index'))
 
+def recuperarregistro(ix):
+    ix = int(ix)
+    fe = open(ficheromovimientos, 'r')
+    csvreader = csv.reader(fe, delimiter=',', quotechar='"')
 
+    contador = 1
+    for linea in csvreader:
+        if contador == ix:
+            fe.close()
+            return linea, ix
+        contador += 1
+
+    fe.close()
+
+    
+def modificarregistro(values):
+    fe = open(ficheromovimientos, 'r')
+    fs = open(ficheronuevo, 'w')
+    ix = int(values.get('ix'))
+
+    precioUnitario = float(values['cantidadPagada'])/float(values['cantidadComprada'])
+    registro = '{},"{}",{},{},{},{},{}\n'.format(values['fecha'], 
+                values['concepto'], 
+                values['monedaComprada'], 
+                values['cantidadComprada'], 
+                values['monedaPagada'], 
+                values['cantidadPagada'], 
+                precioUnitario)
+
+    contador = 1
+    for linea in fe:
+        if contador == ix:
+            linea = registro
+        fs.write(linea)
+
+        contador += 1
+
+    fe.close()
+    fs.close()
+
+    remove(ficheromovimientos)
+    rename(ficheronuevo, ficheromovimientos)
+
+    
 def borrar(ix):
     fe = open(ficheromovimientos, 'r')
     fs = open(ficheronuevo, 'w')
